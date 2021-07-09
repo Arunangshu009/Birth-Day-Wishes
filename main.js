d028a4126368a87fa173f106ed08c2a470238e8f
Arunangshu009 <83114829+Arunangshu009@users.noreply.github.com>
2021-07-09 21:03:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function playsound(){
+ onclick =  ("Button_my");
+ playsound.birthday_song;
+}
+
+function new_image()
+{
+fabric. Image.fromURL('birthday mom.jpg', function (Img) { block_image_object = Img;
+block_image_object.scaleToWidth(700); 
+block_image_object.scaleToHeight (510); 
+block_image_object.set({
+top:0,
+left:1
+});
+canvas.add(block_image_object);
+});
+}
